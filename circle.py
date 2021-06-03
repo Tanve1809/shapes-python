e879b319c544412aa50084046be11d9f2236e825
Tanve Sribharath <69785947+Tanve1809@users.noreply.github.com>
2021-06-03 16:34:44 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,5 @@
+import turtle
+
+cir = turtle.Turtle()
+cir.circle(100)
+turtle.done
---
+++ triangle.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.speed(1)
+triangle = turtle.Turtle()
+for i in range(3):
+    triangle.forward(100)
+    triangle.left(120)
+    turtle.done
